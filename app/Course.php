71d537260ea10264b2799a1471aeccf2c8ac2a03
Nop <nanop_777@hotmail.com>
2019-04-19 10:29:41 +0700

    Edit Model
    Course And Status

--- app/Course.php
+++ app/Course.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+  public $timestamps = false;
+  public $table = "student";
+  protected $primaryKey = 'course_id';
+  protected $fillable = [
+    'course_id','course_name'
+  ];
 }
--- app/Status.php
+++ app/Status.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
-    //
+  public $timestamps = false;
+  public $table = "status";
+  protected $primaryKey = 'status_id';
+  protected $fillable = [
+      'status_id','status_name'
+  ];
 }
