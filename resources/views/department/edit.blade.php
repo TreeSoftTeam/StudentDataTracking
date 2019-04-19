2c50db5f016345a8b09bad6e4a8756d40e279b86
Songrak <songrak2012@hotmail.com>
2019-04-19 11:25:53 +0700

    Add View
    department , faculty

---
+++ resources/views/department/edit.blade.php
@@ -0,0 +1,6 @@
+<html>
+        <form>
+                Department <br>
+                <input type="text" name="department" size="10" maxlength="25">
+                <input type="submit" name="OK" placeholder="Enter Faculty" value="Edit">
+            </form>
---
+++ resources/views/faculty/create.blade.php
@@ -0,0 +1,6 @@
+<html>
+    <form>
+        Faculty <br>
+        <input type="text" name="faculty" size="10" maxlength="25">
+        <input type="submit" name="OK" placeholder="Enter Faculty" value="Add">
+    </form>
