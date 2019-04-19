50f27c9d73b14fa783250ba1e730f7ed0272c81b
Songrak <songrak2012@hotmail.com>
2019-04-19 10:41:50 +0700

    Edit Model
    Department and Faculty

--- app/Department.php
+++ app/Department.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
-    //
+    public $timestamps = false;
+    public $table = "department";
+    protected $primaryKey = 'department_id';
+    protected $fillable = ['department_id','department_name','faculty_id'];
 }
--- app/Faculty.php
+++ app/Faculty.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Faculty extends Model
 {
-    //
+    public $timestamps = false;
+    public $table = "faculty";
+    protected $primaryKey = 'faculty_id';
+    protected $fillable = ['faculty_name'];
 }
