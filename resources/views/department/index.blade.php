01dc7ba9d5251c4ba231908ef1bd03f4532400be
Songrak <songrak2012@hotmail.com>
2019-04-21 22:23:17 +0700

    Index
    Index faculty & department
    (งงๆ)

---
+++ resources/views/department/index.blade.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+    Welcome to index Department
+    <a href="/student/{{$StudentDataList->student_code}}">ดูข้อมูล</a><a href="/student/{{$StudentDataList->student_code}}/edit">แก้ไขข้อมูล</a>
+                    
+</body>
