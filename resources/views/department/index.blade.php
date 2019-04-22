8b4d2ce47f94fcfa9cbeacbdae4fe529ba9689ea
Songrak <songrak2012@hotmail.com>
2019-04-22 09:22:56 +0700

    Edit index
    Faculty & Department

--- resources/views/department/index.blade.php
+++ resources/views/department/index.blade.php
@@ -1,6 +1,83 @@
-<html>
-<body>
-    Welcome to index Department
-    <a href="/student/{{$StudentDataList->student_code}}">ดูข้อมูล</a><a href="/student/{{$StudentDataList->student_code}}/edit">แก้ไขข้อมูล</a>
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        Welcome to index manage department
+        <div class="col-md-9 col-lg-9 col-sm-3 pull-left">
+            <ul class="list-group">
+                @foreach($DepartmentDataList as $DepartmentDataList)
+                    <li class="list-group-item"><a>{{$DepartmentDataList->department_code}}</a> <a>{{$DepartmentDataList->fullname}} <a href="/department/{{$DepartmentDataList->department_code}}">ดูข้อมูล</a><a href="/department/{{$DepartmentDataList->department_code}}/edit">แก้ไขข้อมูล</a>
                     
-</body>
+                    <form action="{{route('department.destroy', $DepartmentDataList->department_code)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger" type="submit">Delete</button>
+                    </form>
+                    
+                    </li>
+                @endforeach
+            </ul>
+    </body>
+</html>
